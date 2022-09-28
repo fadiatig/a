609cd077574aed28c026bbf5183b030f372b0b84
atigfadi <fadiatig3@gmail.com>
2022-09-28 11:34:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Profile from './profile/Profile';
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <div className="container">
+      <Profile name="atig fadi" bio="experience since " profession="seo" imag="C:\Users\Fadi\pro\src\profile\image\ff.webp" phone="+216 97591684"></Profile>
+
+          </div>
+          </div>
+   
+  );
+  }
+
+
+export default App;
+
+
